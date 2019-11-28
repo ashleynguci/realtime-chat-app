e5bba89722d8193387699fab3b8d7b1f29009fab
Ashley Ng <ashley.ng@quanturi.com>
2019-11-28 18:07:31 +0200

    implementing userIDs

--- chat-socket.io/screens/HomeScreen.js
+++ chat-socket.io/screens/HomeScreen.js
@@ -1,65 +1,32 @@
 
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import io from 'socket.io-client';
 import { GiftedChat } from "react-native-gifted-chat";
 export default function HomeScreen() {
-    const [sentMessage, setSentMessage] = useState("");
+
     const [recvMessage, setRecvMessage] = useState([]);
     const socket = useRef(null);
     useEffect(() => {
         socket.current = io("http://10.213.224.58:3001");
         socket.current.on("message", message => {
-            const textMessage = {
-                _id: 3,
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
-                }
-            }
-            textMessage.text = message;
-            setRecvMessage(prevState => [textMessage, ...prevState]);
+
+            setRecvMessage(prevState => GiftedChat.append(prevState, message));
         });
-        setRecvMessage([
-            {
-                _id: 1,
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
-            },
-            {
-                _id: 2,
-                text: 'Hello Ashley',
-                createdAt: new Date(),
-                user: {
-                    _id: 1,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
-            },
-        ])
+
     }, [])
 
     const onSend = (messages) => {
         console.log(messages);
         socket.current.emit("message", messages[0].text);
-        //setSentMessage("");
+        setRecvMessage(prevState => GiftedChat.append(prevState, messages));
+
     }
 
-    //const showRecvMessage = recvMessage.map(msg => (<Text key={msg}>{msg}</Text>))
+
 
     return (
         <View style={{ flex: 1 }}>
-            {/* //<View style={styles.container}> */}
-            {/* {showRecvMessage} 
-         <TextInput value={sentMessage} onChangeText={(text) => setSentMessage(text)} placeholder="Enter chat message..." onSubmitEditing={sendMessage} />  */}
             <GiftedChat
                 messages={recvMessage}
                 onSend={messages => onSend(messages)}
@@ -74,11 +41,4 @@ export default function HomeScreen() {
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,10 +1,31 @@
 const io = require("socket.io")();
+let currentUserId = 2;
+let currentMessageId = 1;
+const userIds = {};
 
+function createMessage(userId, messageText) {
+    return {
+        _id: currentMessageId++,
+        text: messageText,
+        createdAt: new Date(),
+        user: {
+            _id: userId,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+        }
+    }
+}
 io.on("connection", socket => {
     console.log("connected");
-    socket.on("message", message => {
+    console.log(socket.id);
+    userIds[socket.id] = currentUserId++;
+    socket.on("message", textMessage => {
+
+        const userId = userIds[socket.id];
+        const message = createMessage(userId, textMessage)
         console.log(message);
-        io.emit("message", message);
+        socket.broadcast.emit("message", message)
+
     })
 })
 io.listen(3001);
