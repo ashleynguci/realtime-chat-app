70a16a288d397d1727ee989923e1edc27d12bb01
Ashley Ng <ashley.ng@quanturi.com>
2019-12-02 23:58:00 +0200

    display useronline in flatlist

--- chat-socket.io/screens/OnlineListScreen.js
+++ chat-socket.io/screens/OnlineListScreen.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View, FlatList } from 'react-native'
+import { useSelector } from 'react-redux';
 export default function OnlineListScreen() {
+    const onlineList = useSelector(state => state.onlineList);
+    console.log(onlineList);
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text> Online Now </Text>
+            <FlatList data={onlineList} renderItem={({ item }) => { console.log("item", item); return <Text>{item.username}</Text> }}
+                keyExtractor={item => item.username}
+
+            ></FlatList>
         </View>
     )
 }
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,23 +1,33 @@
 const io = require("socket.io")();
 const messageHandler = require("./handlers/message.handlers");
-
+const uuidv1 = require("uuid/v1");
 const users = {};
-let currentUserId = 2;
+
 
 const createUserAvatar = () => {
     const ran1 = Math.round(Math.random() * 200 + 100);
     const ran2 = Math.round(Math.random() * 200 + 100);
     return `https://placeimg.com/${ran1}/${ran2}/any`
 };
+const createUserOnline = () => {
+    const values = Object.values(users);
+    const usernameFilter = values.filter(u => u.username !== undefined);
+    return usernameFilter;
+}
+
 io.on("connection", socket => {
     console.log("a user connected");
     console.log(socket.id);
-    users[socket.id] = { userId: currentUserId++ };
+    users[socket.id] = { userId: uuidv1() };
     socket.on("join", username => {
         users[socket.id].username = username;
         users[socket.id].avatar = createUserAvatar();
         messageHandler.handleMessage(socket, users);
     });
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("action", { type: "users_online", data: createUserOnline() })
+    })
     socket.on("action", action => {
         switch (action.type) {
             case "server/hello":
@@ -28,9 +38,8 @@ io.on("connection", socket => {
                 console.log("Got signin event", action.data);
                 users[socket.id].username = action.data;
                 users[socket.id].avatar = createUserAvatar();
-                const values = Object.values(users);
-                const usernameFilter = values.filter(u => u.username !== undefined);
-                io.emit("action", { type: "users_online", data: usernameFilter })
+
+                io.emit("action", { type: "users_online", data: createUserOnline() })
                 break;
         }
     })
