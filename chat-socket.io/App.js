8122ff7ee1ad331b7eaacd80a16afc5c71989af8
Ashley Ng <ashley.ng@quanturi.com>
2019-12-05 02:05:34 +0200

    awwwwww cant find the bug :<

--- chat-socket.io/App.js
+++ chat-socket.io/App.js
@@ -17,12 +17,13 @@ const reducer = (state = {}, action) => {
   switch (action.type) {
     case "message":
       return { ...state, message: action.data };
-    case "users_online":
-      return { ...state, userOnline: action.data }
+    case "online_List":
+      return { ...state, onlineList: action.data }
     default:
       return state;
   }
 }
+
 const store = applyMiddleware(socketIoMiddleware)(createStore)(reducer);
 
 store.subscribe(() => {
--- chat-socket.io/screens/OnlineListScreen.js
+++ chat-socket.io/screens/OnlineListScreen.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Text, View, FlatList } from 'react-native'
+import { Text, View } from 'react-native'
 import { useSelector } from 'react-redux';
+import { FlatList } from 'react-native-gesture-handler';
+
 export default function OnlineListScreen() {
     const onlineList = useSelector(state => state.onlineList);
-    console.log(onlineList);
+
+    console.log("why", onlineList);
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             <FlatList data={onlineList} renderItem={({ item }) => { console.log("item", item); return <Text>{item.username}</Text> }}
                 keyExtractor={item => item.username}
 
             ></FlatList>
+
         </View>
     )
 }
