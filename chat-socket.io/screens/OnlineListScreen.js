76e344d0f7fe89c7cfab97a4736e79d400a80c41
Ashley Ng <ashley.ng@quanturi.com>
2019-12-06 15:36:06 +0200

    adding UI for app

--- chat-socket.io/screens/OnlineListScreen.js
+++ chat-socket.io/screens/OnlineListScreen.js
@@ -5,15 +5,24 @@ import { useSelector } from 'react-redux';
 
 export default function OnlineListScreen({ navigation }) {
     const onlineList = useSelector(state => state.onlineList);
-    const { itemContainerStyle, imageStyle, usernameStyle } = styles;
+    const { itemContainerStyle, imageStyle, usernameStyle, arrowStyle, usernameText, loggedInText, activeHeadline } = styles;
 
     return (
-
         <View style={{ flex: 1 }}>
+            <Text style={activeHeadline}>Active</Text>
             <FlatList data={onlineList} renderItem={({ item }) => {
-                console.log("item", item); return <TouchableOpacity onPress={() => navigation.navigate("Chat", { name: item.username, userId: item.userId })}><View style={itemContainerStyle}>
-                    <Image style={imageStyle} source={{ uri: item.avatar }} />
-                    <View style={usernameStyle}><Text>{item.username}</Text></View></View></TouchableOpacity>
+                console.log("item", item);
+                return <TouchableOpacity onPress={() => navigation.navigate("Chat", { name: item.username, userId: item.userId })}>
+                    <View style={itemContainerStyle}>
+                        <Image style={imageStyle} source={{ uri: item.avatar }} />
+                        <View style={usernameStyle}>
+                            <Text style={usernameText}>{item.username}</Text>
+                            <Text style={loggedInText}>Logged in {Date()}</Text>
+                        </View>
+                        <Image style={arrowStyle} source={require("../assets/arrow.png")} resizeMode="contain" />
+
+                    </View>
+                </TouchableOpacity>
             }}
                 keyExtractor={item => item.userId}
 
@@ -25,9 +34,59 @@ export default function OnlineListScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-    itemContainerStyle: { flex: 1, flexDirection: 'row' },
-    imageStyle: { width: 100, height: 100, borderRadius: 50 },
-    usernameStyle: { flex: 1, justifyContent: "center", alignItems: "center", fontSize: 20 }
+    activeHeadline: {
+        marginLeft: 25,
+        fontWeight: 'bold',
+        fontSize: 35,
+        color: '#333333',
+        marginTop: 15,
+        marginBottom: 10,
+    },
+    itemContainerStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.08,
+        shadowRadius: 4,
+        elevation: 1,
+        borderRadius: 5,
+        marginTop: 10,
+        width: '90%',
+        alignSelf: 'center'
+    },
+    imageStyle: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 15
+    },
+    usernameStyle: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "flex-start",
+    },
+    usernameText: {
+        fontSize: 16,
+        marginLeft: 20,
+        fontWeight: 'bold',
+    },
+    loggedInText: {
+        fontSize: 12,
+        color: '#c9c7c7',
+        marginLeft: 20,
+        marginTop: 5,
+    },
+    arrowStyle: {
+        height: 20,
+        flex: .2,
+        alignSelf: 'center',
+        justifyContent: 'center'
+    }
 });
 
 
--- chat-socket.io/screens/SignInScreen.js
+++ chat-socket.io/screens/SignInScreen.js
@@ -1,30 +1,101 @@
 import React, { useState } from 'react'
-import { View, TextInput, Image, Button, Platform, KeyboardAvoidingView } from 'react-native'
+import { Text, View, TextInput, ImageBackground, Image, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
 
 export default function SignInScreen({ navigation }) {
+
     const dispatch = useDispatch();
     const [username, setUsername] = useState("");
+    const { signInText, descriptionText, signInImage, usernameInput, textButton, signInButton, backgroundImage } = styles;
+
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Image resizeMode="contain" style={{ flex: 1, width: 200, height: 200 }}
-                source={require("../assets/chat-logo.jpg")} />
+        <ImageBackground source={require("../assets/background.png")} style={backgroundImage}>
             <View style={{ flex: 1 }}>
-                <TextInput
-                    onChangeText={text => setUsername(text)}
-                    value={username}
-                    style={{
-                        fontSize: 25, textAlign: "center"
-                    }} placeholder="Enter username..." />
-                <Button title="Sign in" onPress={() => {
-                    dispatch({ type: "server/join", data: username });
-                    navigation.navigate("App")
-                }} />
+                <Image resizeMode="contain" style={signInImage}
+                    source={require("../assets/signIn_cat.png")} />
+                <View style={{ flex: 1 }}>
+                    <Text style={signInText}>Sign In</Text>
+                    <Text style={descriptionText}>Please enter a username to start</Text>
+                    <TextInput
+                        onChangeText={text => setUsername(text)}
+                        value={username}
+                        style={usernameInput} placeholder="Enter username..." />
+                    <TouchableOpacity style={signInButton} onPress={() => {
+                        dispatch({ type: "server/join", data: username });
+                        navigation.navigate("App")
+                    }}>
+                        <Text style={textButton}>Get Started</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <KeyboardAvoidingView behavior="padding" />
+
             </View>
+        </ImageBackground>
 
-            <KeyboardAvoidingView behavior="padding" />
-
-        </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+
+    signInImage: {
+        flex: 1,
+        width: '80%',
+        alignSelf: 'center',
+    },
+
+    signInText: {
+        marginLeft: 25,
+        fontWeight: 'bold',
+        fontSize: 35,
+        color: '#333333'
+    },
+
+    descriptionText: {
+        color: '#333333',
+        fontSize: 14,
+        marginLeft: 25,
+        marginTop: 5,
+    },
+
+    usernameInput: {
+        marginTop: 40,
+        width: '80%',
+        marginLeft: 25,
+        fontSize: 16,
+        fontWeight: 'bold',
+        borderBottomColor: '#E2E2E2',
+        borderStyle: 'solid',
+        borderBottomWidth: 1,
+        paddingBottom: 10,
+    },
+
+    signInButton: {
+        alignSelf: 'center',
+        backgroundColor: '#CF7C77',
+        color: '#333',
+        marginTop: 50,
+        width: '45%',
+        height: 50,
+        borderRadius: 12,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textButton: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+
+});
